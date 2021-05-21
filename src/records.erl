b1a7c62898c34da1a957abe5245ebfcc081ee971
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-21 21:52:34 +0800

    records

---
+++ src/records.erl
@@ -0,0 +1,17 @@
+-module(records).
+-compile(export_all).
+-author("dannypsnl").
+
+-record(robot, {
+  name,
+  type=industrial,
+  hobbies,
+  details=[]
+}).
+
+first_bot() ->
+  #robot{
+    name = "Mechatron",
+    type = handmade,
+    details = ["Moved by a small man inside"]
+  }.
