413e5ba25c373eb23add6e88ed99ed1c17fec709
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-21 21:55:04 +0800

    format records

--- src/records.erl
+++ src/records.erl
@@ -3,15 +3,15 @@
 -author("dannypsnl").
 
 -record(robot, {
-  name,
-  type=industrial,
-  hobbies,
-  details=[]
+    name,
+    type = industrial,
+    hobbies,
+    details = []
 }).
 
 first_bot() ->
-  #robot{
-    name = "Mechatron",
-    type = handmade,
-    details = ["Moved by a small man inside"]
-  }.
+    #robot{
+        name = "Mechatron",
+        type = handmade,
+        details = ["Moved by a small man inside"]
+    }.
